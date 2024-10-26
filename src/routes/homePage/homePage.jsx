b576eca77e834a50799037202844bf216d3b1077
VictorKurata <104841224+VictorKurata@users.noreply.github.com>
2024-10-26 18:56:08 -0300

    Mudança de slogan homePage.jsx

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -6,23 +6,23 @@ function HomePage() {
         <div className="homePage">
             <div className="textContainer">
                 <div className="wrapper">
-                    <h1 className="title">Com o PartiuLar encontre o seu lar universitario</h1>
+                    <h1 className="title">Divida o espaço com quem combina com você.</h1>
                     <p>
-                        Republicas, Aps compartilhados, Kitnets e muito mais
+                        Partiular conecta universitários em busca de moradia compartilhada, facilitando a escolha de colegas de quarto com interesses em comum. Nossa plataforma oferece uma experiência personalizada e segura, onde a convivência harmoniosa é prioridade.
                     </p>
                     <SearchBar />
                     <div className="boxes">
                         <div className="box">
-                            <h1>3</h1>
-                            <h2>Anos de Mercado</h2>
+                            <h1>5+</h1>
+                            <h2>Anos no mercado</h2>
                         </div>
                         <div className="box">
-                            <h1>PREMIADO</h1>
-                            <h2>Pela Revista Veja e OGlobo</h2>
+                            <h1>2000+</h1>
+                            <h2>Casas disponíveis</h2>
                         </div>
                         <div className="box">
-                            <h1>270</h1>
-                            <h2>Repúblicas</h2>
+                            <h1>10000+</h1>
+                            <h2>Usuários</h2>
                         </div>
                     </div>
                 </div>
